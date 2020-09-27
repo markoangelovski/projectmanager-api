41d81899cf87884251a2246d3f2a4021f046c11c
Angelovski <angelovski.marko@gmail.com>
2020-09-28 00:48:05 +0200

    Added done filter to Task Stats

--- src/lib/Helpers/getTasksAggrCond.js
+++ src/lib/Helpers/getTasksAggrCond.js
@@ -1,11 +1,15 @@
 const Task = require("../../api/tasks/v1/tasks.model");
 
 const getTasksAggrCond = async (cond, aggregateCall) => {
-  let { skip, ownerId } = cond;
+  let { skip, ownerId, done } = cond;
+
+  let query = { owner: ownerId };
+
+  if (done !== null) query.done = done;
 
   const [count, docs] = await Promise.all([
-    Task.countDocuments({ owner: ownerId }),
-    aggregateCall({ skip, ownerId })
+    Task.countDocuments(query),
+    aggregateCall(skip, query)
   ]);
 
   let remaining = count - (skip + 20);
