a984023a6c023fd6ba05f8b73b71990a16cad10b
Angelovski <angelovski.marko@gmail.com>
2022-06-08 22:02:55 +0200

    Patch locale empty GTM key validation fix

--- src/validation/gtmAttributes.js
+++ src/validation/gtmAttributes.js
@@ -3,10 +3,10 @@ module.exports = locale => {
 
   const localeKeys = Object.keys(locale);
 
-  // Check all keys stored in the Locale. Compare keys from Locale and Scanned GTM. If both keys exist and they are not equal, store the different keys in result object.
+  // Check all keys stored in the Locale. Compare keys from Locale and Scanned GTM. If both keys exist, have some data to them and they are not equal, store the different keys in result object.
   localeKeys.forEach(key => {
     if (locale.GTM.hasOwnProperty(key)) {
-      if (locale[key] !== locale.GTM[key]) {
+      if (locale.GTM[key].length && locale[key] !== locale.GTM[key]) {
         result[key] = {};
         result[key].previous = locale[key];
         result[key].scanned = locale.GTM[key];
